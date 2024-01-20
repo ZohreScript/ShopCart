03ea88919afb366ce71b0b8b5743d4dee174bf8a
zohreh omidi <121631510+ZohreScript@users.noreply.github.com>
2024-01-20 23:20:39 +0330

    Update Products.jsx

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -7,7 +7,7 @@ function Products() {
     <>
 
       <p className='font-semibold items-start text-left text-lg m-3'>Welcome to my Clothing Shop</p>
-      <div className='grid grid-cols-2 gap-10'>
+      <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-10'>
       {ShopData.map((data, index) => (
         <ProductCard key={index} {...data} />
       ))}
